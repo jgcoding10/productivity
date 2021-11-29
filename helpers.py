8ad0d1360099ad613b50c13d7a723e98e8f1645e
JackGriffin <jackgriffin@college.harvard.edu>
2021-11-29 15:24:14 -0500

    Cleaned out stuff from finance that wasn't helpful

--- helpers.py
+++ helpers.py
@@ -43,84 +43,10 @@ def after_request(response):
 @app.route("/")
 @login_required
 def index():
-    """Show portfolio of stocks"""
+   
+    # Fill this with relavent code
 
-    # Query database for user's cash
-    rows = db.execute("SELECT cash FROM users WHERE id = :id", id=session["user_id"])
-    if not rows:
-        return apology("missing user")
-    cash = rows[0]["cash"]
-    total = cash
-
-    # Query database for user's stocks
-    stocks = db.execute("""SELECT symbol, SUM(shares) AS shares FROM transactions
-        WHERE user_id = :user_id GROUP BY symbol HAVING SUM(shares) > 0""", user_id=session["user_id"])
-
-    # Query Yahoo for stocks' latest names and prices
-    for stock in stocks:
-        quote = lookup(stock["symbol"])
-        stock["name"] = quote["name"]
-        stock["price"] = quote["price"]
-        total += stock["shares"] * quote["price"]
-
-    # Render portfolio
-    return render_template("index.html", cash=cash, stocks=stocks, total=total)
-
-
-@app.route("/buy", methods=["GET", "POST"])
-@login_required
-def buy():
-    """Enable user to buy a stock."""
-
-    # POST
-    if request.method == "POST":
-
-        # Validate form submission
-        if not request.form.get("symbol"):
-            return apology("missing symbol")
-        elif not request.form.get("shares"):
-            return apology("missing shares")
-        elif not request.form.get("shares").isdigit():
-            return apology("invalid shares")
-        shares = int(request.form.get("shares"))
-        if not shares:
-            return apology("too few shares")
-
-        # Get stock quote
-        quote = lookup(request.form.get("symbol"))
-        if not quote:
-            return apology("invalid symbol")
-
-        # Cost to buy
-        cost = shares * quote["price"]
-
-        # Get user's cash balance
-        rows = db.execute("SELECT cash FROM users WHERE id = :id", id=session["user_id"])
-        if not rows:
-            return apology("missing user")
-        cash = rows[0]["cash"]
-
-        # Ensure user can afford
-        if cash < cost:
-            return apology("can't afford")
-
-        # Record purchase
-        db.execute("""INSERT INTO transactions (user_id, symbol, shares, price)
-            VALUES(:user_id, :symbol, :shares, :price)""",
-                   user_id=session["user_id"], symbol=quote["symbol"], shares=shares, price=quote["price"])
-
-        # Deduct cash
-        db.execute("UPDATE users SET cash = cash - :cost WHERE id = :id",
-                   cost=cost, id=session["user_id"])
-
-        # Display portfolio
-        flash("Bought!")
-        return redirect("/")
-
-    # GET
-    else:
-        return render_template("buy.html")
-
+    return render_template("index.html")
 
 @app.route("/check", methods=["GET"])
 def check():
@@ -139,9 +65,13 @@ def check():
 @app.route("/history")
 @login_required
 def history():
-    """Display user's history of transactions."""
-    transactions = db.execute(
-        "SELECT * FROM transactions WHERE user_id = :user_id", user_id=session["user_id"])
+
+    # Could use this later
+
+    # """Display user's history of transactions."""
+    # transactions = db.execute(
+    #     "SELECT * FROM transactions WHERE user_id = :user_id", user_id=session["user_id"])
+
     return render_template("history.html", transactions=transactions)
 
 
@@ -192,32 +122,6 @@ def logout():
     # Redirect user to login form
     return redirect("/login")
 
-
-@app.route("/quote", methods=["GET", "POST"])
-@login_required
-def quote():
-    """Get a stock quote."""
-
-    # POST
-    if request.method == "POST":
-
-        # Validate form submission
-        if not request.form.get("symbol"):
-            return apology("missing symbol")
-
-        # Get stock quote
-        quote = lookup(request.form.get("symbol"))
-        if not quote:
-            return apology("invalid symbol")
-
-        # Display quote
-        return render_template("quoted.html", quote=quote)
-
-    # GET
-    else:
-        return render_template("quote.html")
-
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user for an account."""
@@ -251,63 +155,6 @@ def register():
     # GET
     else:
         return render_template("register.html")
-
-@app.route("/sell", methods=["GET", "POST"])
-@login_required
-def sell():
-    """Enable user to sell a stock."""
-
-    # POST
-    if request.method == "POST":
-
-        # Validate form submission
-        if not request.form.get("symbol"):
-            return apology("missing symbol")
-        symbol = request.form.get("symbol").upper()
-        if not request.form.get("shares"):
-            return apology("missing shares")
-        elif not request.form.get("shares").isdigit():
-            return apology("invalid shares")
-        shares = int(request.form.get("shares"))
-        if shares < 1:
-            return apology("shares must be positive")
-
-        # Check how many shares user owes
-        rows = db.execute("""SELECT SUM(shares) AS shares FROM transactions
-            WHERE user_id = :user_id AND symbol = :symbol GROUP BY symbol""",
-                          user_id=session["user_id"], symbol=symbol)
-        if len(rows) != 1:
-            return apology("symbol not owned")
-        if shares > rows[0]["shares"]:
-            return apology("too many shares")
-
-        # Get stock quote
-        quote = lookup(request.form.get("symbol"))
-
-        # Record sale
-        db.execute("""INSERT INTO transactions (user_id, symbol, shares, price)
-            VALUES(:user_id, :symbol, :shares, :price)""",
-                   user_id=session["user_id"], symbol=quote["symbol"], shares=-shares, price=quote["price"])
-
-        # Deposit cash
-        db.execute("UPDATE users SET cash = cash + :value WHERE id = :id",
-                   value=shares * quote["price"], id=session["user_id"])
-
-        # Display portfolio
-        flash("Sold!")
-        return redirect("/")
-
-    # GET
-    else:
-
-        # Get symbols owned
-        rows = db.execute("""SELECT symbol FROM transactions
-            WHERE user_id = :user_id GROUP BY symbol HAVING SUM(shares) > 0""",
-                          user_id=session["user_id"])
-        symbols = [row["symbol"] for row in rows]
-
-        # Display sales form
-        return render_template("sell.html", symbols=symbols)
 
 
 def errorhandler(e):
